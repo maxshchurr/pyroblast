573074f0f9e814d803b71d093a894dc097a822cc
Max Shchur <mxm.shchur@gmail.com>
2022-11-25 18:17:28 +0200

    imported cohere and implemented embeddings

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from pyrogram import Client
+import cohere
+
 """
 So to start with Pyrogram we have to make some manipulations.
 1 step - visit https://my.telegram.org/auth and login with your phone number.
@@ -6,6 +8,9 @@ So to start with Pyrogram we have to make some manipulations.
 There you'll find App api_id and App api_hash.These fields are used below (api_id and api_hash).
 We will need them only once for the first running.
 To run script we use this line:app = Client("pyroApp", api_id=api_id, api_hash=api_hash)
+
+More about Pyrogram: https://docs.pyrogram.org/
+More about cohere: https://docs.cohere.ai/docs
 """
 
 # api_id = your api_id
@@ -16,12 +21,20 @@ To run script we use this line:app = Client("pyroApp", api_id=api_id, api_hash=a
 
 group = "me"
 app = Client("pyroApp")
-all_msg = []
+all_msg = ['hello', 'hii']
 with app:
     for message in app.get_chat_history(group):
+        if message.text is not None:
+            all_msg.append(message.text)
 
-        all_msg.append(message.text)
 
+co = cohere.Client('R3ZspcOxRQuTMdS5i0UPR2Ozo0shjRmUAEsOoFoe')
 
-print(all_msg)
+response = co.embed(
+    model='small',
+    texts=all_msg
+)
+
+print(response)
+
 # app.run()
