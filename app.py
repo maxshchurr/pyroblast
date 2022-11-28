aebb3194f923771cd7f158320347b5f8e1e8f7fb
Max Shchur <mxm.shchur@gmail.com>
2022-11-28 20:47:05 +0200

    added description

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from pyrogram import Client
 import cohere
 import weaviate # to communicate to the Weaviate instance
-from weaviate.wcs import WCS
-
 
 """
 So to start with Pyrogram we have to make some manipulations.
-1 step - visit https://my.telegram.org/auth and login with your phone number.
-2 step - after successful authentication you will be redirected to a page. 
+Visit https://my.telegram.org/auth and login with your phone number.
+After successful authentication you will be redirected to a page. 
 There you'll find App api_id and App api_hash.These fields are used below (api_id and api_hash).
 We will need them only once for the first running.
 To run script we use this line:app = Client("pyroApp", api_id=api_id, api_hash=api_hash)
@@ -15,6 +13,62 @@ To run script we use this line:app = Client("pyroApp", api_id=api_id, api_hash=a
 More about Pyrogram: https://docs.pyrogram.org/
 More about cohere: https://docs.cohere.ai/docs
 More about weaviate: https://weaviate.io/
+"""
+
+"""
+Step 1
+
+api_id = 23434761
+api_hash = "58349ea9b93ba4235f632a706fe149bc"
+
+app = Client("my_account", api_id=api_id, api_hash=api_hash)
+
+app.run()
+
+
+Step 2
+
+Sign In https://console.semi.technology/ and create a Weaviate Cluster
+Connect to this cluster from web
+
+
+Step 3 
+
+Here is cluster's name that you have change to yours  
+in models.py
+client = weaviate.Client("https://your_cluster.semi.network")
+
+and in app.py
+client_url = "https://your_cluster.semi.network"
+
+Then run models.py to create classes
+
+
+Step 4 
+
+If you want to use cohere visit this site https://docs.cohere.ai/reference/embed and copy authentication string 
+
+co = cohere.Client('your_auth')
+
+Step 5
+
+Run app.py
+
+Step 6
+
+Visit https://console.semi.technology/console/query
+
+You'll see your schema and query tool
+To get extracted data run this in query tool
+
+ {
+  Get{
+    Posts{
+      description
+    }
+  }
+}
+
 """
 
 # api_id = your api_id
@@ -69,7 +123,7 @@ def add_posts(data, db_url):
 
 
 # Defining the group and retrieving posts from the group
-group = "MargulanSeissembai"
+group = "me"
 data = get_posts_from_the_channel(group)
 
 
